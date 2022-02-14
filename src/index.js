66e4e827eeb29fe860bd737137907efbb0047f4e
Andy Sedzifa <andyaseye@gmail.com>
2022-02-14 09:43:53 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Landing from './landing';
 import reportWebVitals from './reportWebVitals';
+import News from './news.js';
+import Discount from './discount';
+import Trailers from './trailers';
+import Video from './video';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Landing />
+    <Video />
+    <News />
+    <Discount />
+    <Trailers />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +23,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
